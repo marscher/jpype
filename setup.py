cbd6f0c30fe53c93f928ff6bb5083cad3d399977
marscher <m.scherer@fu-berlin.de>
2014-08-31 23:43:42 +0200

    [setup] use jni.h from given JAVA_HOME, then fallback to distributed headers.

--- setup.py
+++ setup.py
@@ -22,20 +22,44 @@ def find_sources():
                 cpp_files.append(os.path.join(dirpath, filename))
     return cpp_files
 
+
 platform_specific = {
     'include_dirs': [
         os.path.join('src', 'native', 'common', 'include'),
         os.path.join('src', 'native', 'python', 'include'),
-        os.path.join('src', 'native', 'jni_include'),
     ],
     'sources': find_sources(),
 }
 
+fallback_jni = os.path.join('src', 'native', 'jni_include')
+
+# try to include JNI first from eventually given JAVA_HOME, then from distributed
+java_home = os.getenv('JAVA_HOME')
+if java_home:
+    platform_specific['include_dirs'] += [os.path.join(java_home, 'include')]
+    
+    # check if jni.h can be found
+    found = False
+    for d in platform_specific['include_dirs']:
+        if os.path.exists(os.path.join(d, 'jni.h')):
+            found = True
+    if not found:
+        import warnings
+        warnings.warn('Falling back to provided JNI headers, since your provided'
+                      ' JAVA_HOME %s does not provide jni.h' % java_home)
+        platform_specific['include_dirs'] += [fallback_jni]
+    
+else:
+    platform_specific['include_dirs'] += [fallback_jni]
+
 if sys.platform == 'win32':
     platform_specific['libraries'] = ['Advapi32']
-    platform_specific['library_dir'] = [os.path.join(java_home, 'lib'), ]
     platform_specific['define_macros'] = [('WIN32', 1)]
     platform_specific['extra_compile_args'] = ['/EHsc']
+    if java_home:
+        platform_specific['include_dirs'] += os.path.join(java_home, 'include', 'win32')
+        # TODO: investigate if this is really neccessary, since it will fail if java_home is None
+        platform_specific['library_dir'] = [os.path.join(java_home, 'lib')]
 
 elif sys.platform == 'darwin':
     platform_specific['libraries'] = ['dl']
@@ -43,6 +67,8 @@ elif sys.platform == 'darwin':
     # distutils/extension.py:133: UserWarning: Unknown Extension options: 'library_dir'
     #platform_specific['library_dir'] = [os.path.join(java_home, 'Libraries')]
     platform_specific['define_macros'] = [('MACOSX', 1)]
+    if java_home:
+        platform_specific['include_dirs'] += os.path.join(java_home, 'include', 'darwin')
 else: # linux
     platform_specific['libraries'] = ['dl']
 

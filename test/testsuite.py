283370558792c78045eab911cc10022bbb45ac45
marscher <m.scherer@fu-berlin.de>
2014-09-03 19:32:45 +0200

    [testsuite] import unittest2

--- test/testsuite.py
+++ test/testsuite.py
@@ -14,7 +14,7 @@
 #   limitations under the License.
 #   
 #*****************************************************************************
-import unittest
+import unittest2 as unittest
 
 import jpype
 import os.path

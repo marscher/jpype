3bd34a04761bc3d07f68a6dccadcafec7a87db7d
marscher <m.scherer@fu-berlin.de>
2014-09-07 19:37:18 +0200

    syntax

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -233,7 +233,7 @@ $webclient = New-Object System.Net.WebClient
 $url="https://raw.githubusercontent.com/apache/ant/master/bootstrap.bat"
 $filepath = "bootstrap.bat"
 $webclient.DownloadFile($url, $filepath)
-RunCommand("cmd.exe", $filepath)
+RunCommand "cmd.exe" $filepath
 }
 
 function InstallAnt_broken() {

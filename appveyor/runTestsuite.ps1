81c7654f76f7a1a9c5484d4d5d74244d3c88e025
marscher <m.scherer@fu-berlin.de>
2014-09-19 23:13:04 +0200

    removed ;

--- appveyor/runTestsuite.ps1
+++ appveyor/runTestsuite.ps1
@@ -2,33 +2,33 @@ function xslt_transform($xml, $xsl, $output)
 {
 	trap [Exception]
 	{
-	    Write-Host $_.Exception;
+	    Write-Host $_.Exception
 	}
 	
-	$xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
-	$xslt.Load($xsl);
-	$xslt.Transform($xml, $output);
+	$xslt = New-Object System.Xml.Xsl.XslCompiledTransform
+	$xslt.Load($xsl)
+	$xslt.Transform($xml, $output)
 }
 
 function upload($file) {
     trap [Exception]
     {
-        Write-Host $_.Exception;
+        Write-Host $_.Exception
     }
 
-    $wc = New-Object 'System.Net.WebClient';
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $file); 
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $file)
 }
 
 function run {
-    $stylesheet = "C:/projects/jpype/test/transform_xunit_to_appveyor.xsl";
+    $stylesheet = "C:/projects/jpype/test/transform_xunit_to_appveyor.xsl"
     $output = "C:/projects/jpype/test/transformed.xml"
     
     nosetests test/jpypetest --all-modules --with-xunit
-    $success = $?;
+    $success = $?
     Write-Host "result code of nosetests:" $success
     
-    $input = "C:/projects/jpype/nosetests.xml";
+    $input = "C:/projects/jpype/nosetests.xml"
 
     xslt_transform $input $stylesheet $output
 

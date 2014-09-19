4dcefe4cc6630537ac4f5e624233a78592ea4c4b
marscher <m.scherer@fu-berlin.de>
2014-09-19 13:21:29 +0200

    dbg

--- jpype/_jarray.py
+++ jpype/_jarray.py
@@ -54,6 +54,7 @@ class _JavaArrayClass(object) :
 		
 	def __getslice__(self, i, j) : 
 		print "__getslice__(%s, %s)" %(i,j)
+		print "sys.maxint = %s" %sys.maxint
 		if j is None or j == sys.maxint:
 			j = _jpype.getArrayLength(self.__javaobject__)
 			print "reset j to size of jarray = %s" %j

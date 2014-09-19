d1094f15e743b5f101bd29f5ff6e59b602b614e5
marscher <m.scherer@fu-berlin.de>
2014-09-19 13:47:37 +0200

    [jarray] compare second slice index against sys.maxsize instead of sys.maxint

--- jpype/_jarray.py
+++ jpype/_jarray.py
@@ -53,17 +53,12 @@ class _JavaArrayClass(object) :
 		_jpype.setArrayItem(self.__javaobject__, ndx, val)
 		
 	def __getslice__(self, i, j) : 
-		print "__getslice__(%s, %s)" %(i,j)
-		print "sys.maxint = %s" %sys.maxint
 		if j == sys.maxsize:
-			print "j == sys.maxsize"
-		if j >= sys.maxint:
 			j = _jpype.getArrayLength(self.__javaobject__)
-			print "reset j to size of jarray = %s" %j
 		return _jpype.getArraySlice(self.__javaobject__, i, j)
 		
 	def __setslice__(self, i, j, v) : 
-		if j >= sys.maxint:
+		if j == sys.maxsize:
 			j = _jpype.getArrayLength(self.__javaobject__)
 		_jpype.setArraySlice(self.__javaobject__, i, j, v)
 

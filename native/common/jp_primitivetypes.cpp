d23fb6c2a6fe82ae74a8249abff90db994ae54e8
marscher <m.scherer@fu-berlin.de>
2014-09-24 21:41:45 +0200

    revert

--- native/common/jp_primitivetypes.cpp
+++ native/common/jp_primitivetypes.cpp
@@ -591,7 +591,7 @@ HostRef* JPBooleanType::asHostObjectFromObject(jvalue val)
 
 EMatchType JPBooleanType::canConvertToJava(HostRef* obj)
 {
-	if (JPEnv::getHost()->isInt(obj) || JPEnv::getHost()->isLong(obj))
+	if (JPEnv::getHost()->isInt(obj))
 	{
 		return _implicit;
 	}
